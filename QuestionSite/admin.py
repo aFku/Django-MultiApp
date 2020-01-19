995d9517aaf8f435eff19ecfcecb602371036916
aFku <44970719+aFku@users.noreply.github.com>
2020-01-19 19:58:30 +0100

    end of polls tutorial
    Lets re-design all apps

--- QuestionSite/admin.py
+++ QuestionSite/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Question, Answer
 
-"""Adding Question model to admin panel. Now we can create this from there."""
 
-admin.site.register(Question)
-admin.site.register(Answer)
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Text', {'fields': ['question_text', 'question_description']}),
+        ('Data information', {'fields': ['question_pub_date']})
+    ]
+    inlines = [AnswerInline]
+    list_display = ('question_text', 'question_pub_date', 'was_published_recently')
+    list_filter = ['question_pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- QuestionSite/models.py
+++ QuestionSite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
 
@@ -10,6 +11,13 @@ class Question(models.Model):
     question_text = models.TextField('Question', max_length=100)
     question_description = models.TextField('Description', max_length=255)
     question_pub_date = models.DateTimeField('Date published')
+
+    def was_published_recently(self):
+        return timezone.now() - datetime.timedelta(days=1) <= self.question_pub_date <= timezone.now()
+
+    was_published_recently.admin_order_field = 'question_pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
