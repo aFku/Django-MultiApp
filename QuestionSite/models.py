18423af3791a8a4f28039606cb2d8ddf36a9d5cc
aFku <44970719+aFku@users.noreply.github.com>
2020-02-04 20:36:43 +0100

    Update models.py

--- QuestionSite/models.py
+++ QuestionSite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -44,3 +45,18 @@ class MainComment(CommentInterface):
 
 class SubComment(CommentInterface):
     related_comment = models.ForeignKey(MainComment, on_delete=models.CASCADE)
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.TextField(max_length=100, blank=True)
+    registration_date = models.DateTimeField()
+    first_name = models.TextField(max_length=50, blank=False)
+    last_name = models.TextField(max_length=50, blank=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
